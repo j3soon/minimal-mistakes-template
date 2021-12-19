41957e75a6d0775f7aae322e5a43cb0a6514650b
Johnson <j3.soon@msa.hinet.net>
2021-12-19 23:44:02 +0800

    Tidy up scripts

--- scripts/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-bundle
-# For automatically changing last modified date
-cp ${BASH_SOURCE%/*}/../git-hooks/pre-commit.sh ${BASH_SOURCE%/*}/../.git/hooks/pre-commit
-echo "Installation completed. Run `./preview.sh` to preview locally."
